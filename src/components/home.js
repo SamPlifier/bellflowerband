4baf3195faa35b750bf71b7d017a31c588903dd1
SamDC <sam.davis.castro@gmail.com>
2019-01-26 19:04:21 -0500

    added possible svg

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <div>Home</div>
+            <img src="../src/assets/instruments.svg" alt="6 instruments- voice, guitar, base, drums, hand percussion, keyboards, trumpet"/>
+        </div>
     )
 }
 
