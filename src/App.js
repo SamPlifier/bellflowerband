4824b9c530a4cbeac1d7079594fabbae22fe291f
SamDC <sam.davis.castro@gmail.com>
2019-01-26 22:43:11 -0500

    will try routing updates, routing currently works in this version.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-import Home from './components/home.js';
+import ButtonAppBar from './components/navbar.js';
 import NavMenu from './components/navMenu.js';
+import Home from './components/home.js';
 import Try from './components/try.js';
 import Try2 from './components/try2.js';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
+import grey from '@material-ui/core/colors/grey';
+const theme = createMuiTheme({
+    palette: {
+        primary: teal,
+        secondary: grey
+    },
+    status: {
+        danger: 'yellow'
+    }
+});
+
 
 class App extends Component {
     render() {
         return (
-        <Router>
+            <MuiThemeProvider theme={theme}>
+            <Router>
             <div className="App">
-                <NavMenu />
+                <NavMenu/>
+                <ButtonAppBar />
                 <div>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/try" component={Try} />
-                    <Route exact path="/try2" component={Try2} />
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/try" component={Try}/>
+                    <Route exact path="/try2" component={Try2}/>
                 </div>
             </div>
-        </Router>);
+        </Router>
+    </MuiThemeProvider>);
     }
 }
 
--- src/components/navMenu.js
+++ src/components/navMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
+
 const NavMenu = () => {
     return (
         <div>
