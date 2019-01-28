c53d254bf45f6bed53f81364b639e590ab3b433d
SamDC <sam.davis.castro@gmail.com>
2019-01-27 23:35:03 -0500

    followed terminal message to add homepage to package json for github pages

---
+++ build/precache-manifest.4025ea870330d77889c94efbe9e63d6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e056faa54bd5edba973b",
+    "url": "/bellflowerband/static/css/main.1b0f05aa.chunk.css"
+  },
+  {
+    "revision": "e056faa54bd5edba973b",
+    "url": "/bellflowerband/static/js/main.e056faa5.chunk.js"
+  },
+  {
+    "revision": "e6123e1876b2dea38558",
+    "url": "/bellflowerband/static/js/1.e6123e18.chunk.js"
+  },
+  {
+    "revision": "b9be85c3e21ace16d361",
+    "url": "/bellflowerband/static/js/runtime~main.b9be85c3.js"
+  },
+  {
+    "revision": "5352d9a71e3b92dd33b49c761f01bb40",
+    "url": "/bellflowerband/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57b6fec47aee4cc080b0c7356475e7a2.js"
+  "/bellflowerband/precache-manifest.4025ea870330d77889c94efbe9e63d6f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/bellflowerband/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
