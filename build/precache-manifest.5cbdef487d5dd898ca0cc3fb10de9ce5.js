5db97e40f7c7ceb7cc1bf1295662550f211c2e0b
SamDC <sam.davis.castro@gmail.com>
2019-01-28 19:39:32 -0500

    package.json update, nav menu horizontal and text centered

---
+++ build/precache-manifest.5cbdef487d5dd898ca0cc3fb10de9ce5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3eab888fcc99608b7d24",
+    "url": "/bellflowerband/build/static/css/main.a8ceb38f.chunk.css"
+  },
+  {
+    "revision": "3eab888fcc99608b7d24",
+    "url": "/bellflowerband/build/static/js/main.3eab888f.chunk.js"
+  },
+  {
+    "revision": "f03705cc832ec29bd9f3",
+    "url": "/bellflowerband/build/static/js/1.f03705cc.chunk.js"
+  },
+  {
+    "revision": "ce7164e0247f8ff3936b",
+    "url": "/bellflowerband/build/static/js/runtime~main.ce7164e0.js"
+  },
+  {
+    "revision": "f64a0b2f0d00bbbfb4ffc67a670f856c",
+    "url": "/bellflowerband/build/index.html"
+  }
+];
