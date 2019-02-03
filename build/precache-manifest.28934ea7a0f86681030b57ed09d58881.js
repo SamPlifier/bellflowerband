6ba0796b21778b07b1738b2f554610f2af965d94
SamDC <sam.davis.castro@gmail.com>
2019-02-03 03:59:54 -0500

    added static img address paths for now

---
+++ build/precache-manifest.28934ea7a0f86681030b57ed09d58881.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d3164868978052b2287",
+    "url": "/bellflowerband/build/static/css/main.8e53cd0b.chunk.css"
+  },
+  {
+    "revision": "9d3164868978052b2287",
+    "url": "/bellflowerband/build/static/js/main.9d316486.chunk.js"
+  },
+  {
+    "revision": "2fa43149169e94a6bfc8",
+    "url": "/bellflowerband/build/static/js/1.2fa43149.chunk.js"
+  },
+  {
+    "revision": "ce7164e0247f8ff3936b",
+    "url": "/bellflowerband/build/static/js/runtime~main.ce7164e0.js"
+  },
+  {
+    "revision": "4992ba7b9ec6b4f9c466e7e3556c2531",
+    "url": "/bellflowerband/build/index.html"
+  }
+];
