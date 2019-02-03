b9b957ded3503757fc0176c25d79bc9f2dd51ba4
SamDC <sam.davis.castro@gmail.com>
2019-02-03 00:15:15 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bellflowerband/build/precache-manifest.17fe295dbcb22fa55bddd7b3c233d952.js"
+  "/bellflowerband/build/precache-manifest.a3fa7a1f7688d99df9c1d54cc20cd164.js"
 );
 
 workbox.clientsClaim();
