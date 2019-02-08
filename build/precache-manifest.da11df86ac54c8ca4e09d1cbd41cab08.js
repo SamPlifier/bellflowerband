7b57b80da4416b39acb46b4ac8b856668f4bd4dc
SamDC <sam.davis.castro@gmail.com>
2019-02-08 17:34:16 -0500

    calendar page only storing today + 1month away dates, started componentizing css

---
+++ build/precache-manifest.da11df86ac54c8ca4e09d1cbd41cab08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7adcf4f9fc8c80f37e6",
+    "url": "/bellflowerband/build/static/css/main.11594ce0.chunk.css"
+  },
+  {
+    "revision": "f7adcf4f9fc8c80f37e6",
+    "url": "/bellflowerband/build/static/js/main.f7adcf4f.chunk.js"
+  },
+  {
+    "revision": "e5336e84fcfda9991be6",
+    "url": "/bellflowerband/build/static/js/1.e5336e84.chunk.js"
+  },
+  {
+    "revision": "ce7164e0247f8ff3936b",
+    "url": "/bellflowerband/build/static/js/runtime~main.ce7164e0.js"
+  },
+  {
+    "revision": "f83b1d6bf3ed63d3b72009484ec9306f",
+    "url": "/bellflowerband/build/static/media/_sam3.f83b1d6b.png"
+  },
+  {
+    "revision": "78263f844355e127ff738735e532d9ed",
+    "url": "/bellflowerband/build/index.html"
+  }
+];
--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-
+import styles from '../styles/calendar.css';
+console.log(styles);
 
 class Calendar extends React.Component {
     state = {
@@ -8,14 +9,29 @@ class Calendar extends React.Component {
     }
 
     componentDidMount() {
+        let now = new Date();
+        let startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0);
+        let endOfDayOneMonthOut = new Date(now.getFullYear(), now.getMonth() + 1, now.getDate(), 23, 59, 59);
         axios.get('https://www.googleapis.com/calendar/v3/calendars/thebellflowerband%40gmail.com/events?key=AIzaSyA73ezNBuEPQSSjMgoMjfiFa5wwT1TJht8')
             .then(res => {
-                let events = res.data.items;
-                events = events.sort((a, b) => (Date.parse(a.start.dateTime.toString()) > Date.parse(b.start.dateTime.toString()) ? 1: -1));
-                console.log(events);
-                this.setState({ calendarEvents: events });
+                let allEvents = res.data.items;
+                allEvents = allEvents.sort((a, b) => (Date.parse(a.start.dateTime.toString()) > Date.parse(b.start.dateTime.toString()) ? 1: -1));
+                let futureEvents = [];
+                allEvents.map((calEvent, i) => {
+                    let eventTime = new Date(calEvent.start.dateTime);
+                    if (eventTime > startOfToday && eventTime < endOfDayOneMonthOut) {
+                        futureEvents.push(calEvent);
+                    }
+                    return true;
+                })
+                this.setState({ calendarEvents: futureEvents });
+            })
+            .catch(err => {
+                alert(`Poo- error occurred: ${err}`);
+                console.log(err);
             })
     }
+
     date(utcDate, type) {
         let date = new Date(utcDate);
         const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
@@ -64,9 +80,9 @@ class Calendar extends React.Component {
             this.state.calendarEvents.map((calEvent, i) => {
                 return  <div className="eventCard" key={i}>
                         <div className="dayNameNum"></div>
+                        <p>{calEvent.summary}</p>
                             <p>{this.date(calEvent.start.dateTime, 'day')}, {this.date(calEvent.start.dateTime, 'month')} {this.date(calEvent.start.dateTime, 'dayNum')}</p>
                             <p>{this.date(calEvent.start.dateTime, 'localTime')} - {this.date(calEvent.end.dateTime, 'localTime')}</p>
-                            <p>{calEvent.summary}</p>
                             <p>{calEvent.location}</p>
                         </div>
             })
