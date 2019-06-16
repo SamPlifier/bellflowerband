a2de3b76dbde31708441992349e9e924d317d01d
SamDC <sam.davis.castro@gmail.com>
2019-06-16 17:32:25 -0400

    updated npm maj and min version, check for breaking changes

---
+++ build/precache-manifest.2e95d3e536551799d91834b283f8a010.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b7055f4a3b5a6d47366eb705c536e8",
+    "url": "/bellflowerband/build/index.html"
+  },
+  {
+    "revision": "61cf8a900d5699b60724",
+    "url": "/bellflowerband/build/static/css/main.b137aca7.chunk.css"
+  },
+  {
+    "revision": "5664d549e094a3c67a33",
+    "url": "/bellflowerband/build/static/js/2.ebe3895a.chunk.js"
+  },
+  {
+    "revision": "61cf8a900d5699b60724",
+    "url": "/bellflowerband/build/static/js/main.3d6c8654.chunk.js"
+  },
+  {
+    "revision": "bd2a0adff448da1aee21",
+    "url": "/bellflowerband/build/static/js/runtime~main.5bbced2e.js"
+  },
+  {
+    "revision": "91e9dcab6fdc300dbcf825e0bc7f6312",
+    "url": "/bellflowerband/build/static/media/_cindy.91e9dcab.png"
+  },
+  {
+    "revision": "6909bcf717f68ee28d953ef6ded043a3",
+    "url": "/bellflowerband/build/static/media/_david.6909bcf7.png"
+  },
+  {
+    "revision": "e57fdb11734a832f25fafe5a343cb8c2",
+    "url": "/bellflowerband/build/static/media/_jeff.e57fdb11.png"
+  },
+  {
+    "revision": "c5733a23da50a9b077d2b1ad45c143b1",
+    "url": "/bellflowerband/build/static/media/_natasha.c5733a23.png"
+  },
+  {
+    "revision": "f83b1d6bf3ed63d3b72009484ec9306f",
+    "url": "/bellflowerband/build/static/media/_sam.f83b1d6b.png"
+  },
+  {
+    "revision": "196ded06765ecc5922f482ada68a70c8",
+    "url": "/bellflowerband/build/static/media/franklin.196ded06.png"
+  }
+]);
